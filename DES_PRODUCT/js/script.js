89b467672823ae60996109e6f50e63b273a5fc4c
Anh Trần <tanh11704@gmail.com>
2023-05-16 16:25:22 +0700

    iphone14.html and fix index.html

--- DES_PRODUCT/js/script.js
+++ DES_PRODUCT/js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
     e.preventDefault();
     $(this).find('input[type="radio"]').prop("checked", true);
   });
+
+  $(".btn__zoom").click(function (e) {
+    e.preventDefault();
+    $(".specifications__left__content").toggleClass(
+      "specifications__left__content__collage"
+    );
+    $(".btn").toggleClass("d-none");
+  });
+
+  $(".btn__minimize").click(function (e) {
+    e.preventDefault();
+    $(".specifications__left__content").toggleClass(
+      "specifications__left__content__collage"
+    );
+    $(".btn").toggleClass("d-none");
+  });
 });
